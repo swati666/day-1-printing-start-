249c090aa4eda1e0c31043cc508adaffc96eba2a
swati666 <26264021-swati666@users.noreply.replit.com>
2023-12-26 09:06:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Write your code below this line 👇
+#print("Hello universe, What a beautiful day it is!")
+#print("I feel Replit is one of the best code editor I've come across")
+
+print("Hello universe, What a beautiful day it is!\nI feel Replit is one of the best code editor I've come across")
+
+#STRING MANIPULATION
+
+print("\n\nHello" + "World")
+
+#One line code
+print(len(input("\n\nEnter your name ")))
